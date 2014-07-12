950b4a4c11770453a6c6875819f8451f75b0d6f5
ictrobot <ictrobot@outlook.com>
2014-07-12 11:25:42 +0100

    Forgot 'this'

--- android/src/ethanjones/modularworld/core/platform/android/AndroidLauncher.java
+++ android/src/ethanjones/modularworld/core/platform/android/AndroidLauncher.java
@@ -10,6 +10,6 @@ public class AndroidLauncher extends AndroidApplication {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-    initialize(new ModularWorld(new AndroidCompatibility()), config);
+    initialize(new ModularWorld(new AndroidCompatibility(this)), config);
   }
 }
