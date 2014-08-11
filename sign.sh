53be9f3a709454f526f3e8f4a6e9a48654033879
ictrobot <ictrobot@outlook.com>
2014-08-11 15:39:24 +0100

    Fix sign.sh

--- sign.sh
+++ sign.sh
@@ -3,14 +3,14 @@
 #Align Debug
 echo " "
 echo "Aligning Debug APK"
-$ANDROID_HOME/tools/zipalign -v 4 android/build/apk/android-debug-unaligned.apk android/build/apk/android-debug-aligned.apk
+$ANDROID_HOME/tools/zipalign -v 4 android/build/outputs/apk/android-debug-unaligned.apk android/build/outputs/apk/android-debug-aligned.apk
 
 #Sign Release
-cp android/build/apk/android-release-unsigned.apk android/build/apk/android-release-signed.apk
+cp android/build/outputs/apk/android-release-unsigned.apk android/build/outputs/apk/android-release-signed.apk
 echo " "
 echo "Signing Release APK"
-jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore /keys/EthanJonesApps.keystore -storepass $KEY_STORE_PASS android/build/apk/android-release-signed.apk release 
+jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore /keys/EthanJonesApps.keystore -storepass $KEY_STORE_PASS android/build/outputs/apk/android-release-signed.apk release 
 #Align Release
 echo " "
 echo "Aligning Release APK"
-$ANDROID_HOME/tools/zipalign -v 4 android/build/apk/android-release-signed.apk android/build/apk/android-release-signed-aligned.apk
+$ANDROID_HOME/tools/zipalign -v 4 android/build/outputs/apk/android-release-signed.apk android/build/outputs/apk/android-release-signed-aligned.apk
