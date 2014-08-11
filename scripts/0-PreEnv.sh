ce8fb813e8828510786b390c19143b8b0b35e8cf
ictrobot <ictrobot@outlook.com>
2014-08-11 16:51:20 +0100

    Execute zip with "-q" for quiet"

--- scripts/0-PreEnv.sh
+++ scripts/0-PreEnv.sh
@@ -1,4 +1,4 @@
-zip -r -9 source.zip * -x ".git"
+zip -q -r -9 source.zip * -x ".git"
 
 mv android/assets/version android/assets/version.tmp
 cat android/assets/version.tmp | sed -e "s/%BUILD_NUMBER%/$BUILD_NUMBER/" >> android/assets/version
