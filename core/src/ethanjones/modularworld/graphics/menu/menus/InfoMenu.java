f358a0faad69099bffbf582e32a124cc2f97a14c
ictrobot <ictrobot@outlook.com>
2014-08-21 21:21:54 +0100

    Fix info menu

--- core/src/ethanjones/modularworld/graphics/menu/menus/InfoMenu.java
+++ core/src/ethanjones/modularworld/graphics/menu/menus/InfoMenu.java
@@ -1,7 +1,9 @@
 package ethanjones.modularworld.graphics.menu.menus;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.Align;
 import ethanjones.modularworld.graphics.menu.Menu;
 import ethanjones.modularworld.graphics.menu.MenuTools;
 
@@ -19,13 +21,15 @@ public class InfoMenu extends Menu {
   public InfoMenu(String labelText) {
     super();
     text = new Label(labelText, skin);
+    text.setAlignment(Align.center, Align.bottom);
     button = MenuTools.getBackButton(this);
   }
 
   @Override
   public void resize(int width, int height) {
     super.resize(width, height);
-    
+    text.setBounds(0, Gdx.graphics.getHeight() / 2f, Gdx.graphics.getWidth(), text.getPrefHeight());
+    button.setBounds(Gdx.graphics.getWidth() / 2 - button.getPrefWidth() / 2, Gdx.graphics.getHeight() / 4, button.getPrefWidth(), button.getPrefHeight());
   }
 
   @Override
