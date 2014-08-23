5e9820a27db35948ad1da3c1cdb1525696888078
ictrobot <ictrobot@outlook.com>
2014-08-23 11:57:41 +0100

    Add method to get bounding box

--- core/src/ethanjones/modularworld/block/Block.java
+++ core/src/ethanjones/modularworld/block/Block.java
@@ -1,9 +1,13 @@
 package ethanjones.modularworld.block;
 
+import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.math.collision.BoundingBox;
 import ethanjones.modularworld.core.data.DataGroup;
 import ethanjones.modularworld.graphics.world.BlockTextureHandler;
 
 public abstract class Block {
+  private static BoundingBox fullBlockBoundingBox = new BoundingBox(new Vector3(0, 0, 0), new Vector3(1, 1, 1));
+
   protected BlockTextureHandler textureHandler;
   String mainMaterial;
 
@@ -17,6 +21,10 @@ public abstract class Block {
 
   public BlockTextureHandler getTextureHandler(DataGroup data) {
     return textureHandler;
+  }
+
+  public BoundingBox getBoundingBox() {
+    return fullBlockBoundingBox;
   }
 
 }
