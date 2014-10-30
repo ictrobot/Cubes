4028a0990f92206c1526ea56fdc46224e56497e5
ictrobot <ictrobot@outlook.com>
2014-10-30 21:38:09 +0000

    Fix build script

--- scripts/0-PreEnv.sh
+++ scripts/0-PreEnv.sh
@@ -1,5 +1,5 @@
 zip -q -r -9 source.zip * -x ".git"
 
-mv android/assets/version android/assets/version.tmp
-cat android/assets/version.tmp | sed -e "s/%BUILD_NUMBER%/$BUILD_NUMBER/" >> android/assets/version
-rm -rf android/assets/version.tmp
+mv assets/version assets/version.tmp
+cat assets/version.tmp | sed -e "s/%BUILD_NUMBER%/$BUILD_NUMBER/" >> assets/version
+rm -rf assets/version.tmp
