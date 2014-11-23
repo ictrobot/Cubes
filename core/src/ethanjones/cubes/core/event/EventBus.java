1ca507f59deed2d5a9c8f4206d2faea635266f4d
ictrobot <ictrobot@outlook.com>
2014-11-23 10:58:33 +0000

    Revert "Prevent events being posted multiple times to the same EventHandler"
    This reverts commit 25c5bae8791146bbc6868dadafcec998f3299247.

--- core/src/ethanjones/cubes/core/event/EventBus.java
+++ core/src/ethanjones/cubes/core/event/EventBus.java
@@ -59,7 +59,6 @@ public class EventBus {
   public <E extends Event> E post(E event) {
     if (event == null) return null;
     Class<?> c = event.getClass();
-    final List<EventHandler> posted = new ArrayList<EventHandler>(); //Prevents being posted multiple times to same EventHandler
     while (c != null && Event.class.isAssignableFrom(c)) {
       Class<? extends Event> eventClass = c.asSubclass(Event.class);
       final List<EventHandler> list = getList(eventClass);
@@ -67,10 +66,8 @@ public class EventBus {
         Iterator<EventHandler> iterator = list.iterator();
         while (iterator.hasNext()) {
           EventHandler eventHandler = iterator.next();
-          if (posted.contains(eventHandler)) continue;
-          posted.add(eventHandler);
           try {
-            eventHandler.onEvent(event);
+            ((EventHandler<E>) eventHandler).onEvent(event);
           } catch (Exception e) {
             iterator.remove();
             Log.warning("EventHandler " + eventHandler.getClass().getSimpleName() + " has been removed as it throw an exception while handling " + event.getClass().getSimpleName());
