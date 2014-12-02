c03f7b5b1e72a2eedc5a9eed0d609cce62664c4c
ictrobot <ictrobot@outlook.com>
2014-12-02 20:24:56 +0000

    Remove Block parameter from BlockAttributes

--- core/src/ethanjones/cubes/block/data/BlockAttributes.java
+++ core/src/ethanjones/cubes/block/data/BlockAttributes.java
@@ -4,15 +4,11 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-import ethanjones.cubes.block.Block;
-
 public class BlockAttributes {
 
-  public final Block block;
   public final List<Attribute> attributes;
 
-  public BlockAttributes(Block block, Attribute... attributes) {
-    this.block = block;
+  public BlockAttributes(Attribute... attributes) {
     this.attributes = Collections.unmodifiableList(Arrays.asList(attributes));
   }
 }
