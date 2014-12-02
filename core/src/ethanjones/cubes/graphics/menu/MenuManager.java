889a942c6dfbb7a0143ef064deacca32067bdd2b
ictrobot <ictrobot@outlook.com>
2014-12-02 19:35:39 +0000

    Better random block selector for menu background

--- core/src/ethanjones/cubes/graphics/menu/MenuManager.java
+++ core/src/ethanjones/cubes/graphics/menu/MenuManager.java
@@ -5,11 +5,13 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.utils.Array;
+import java.util.ArrayList;
 import java.util.List;
 
 import ethanjones.cubes.block.Block;
 import ethanjones.cubes.block.Blocks;
 import ethanjones.cubes.core.util.BlockFace;
+import ethanjones.cubes.graphics.world.BlockTextureHandler;
 
 public class MenuManager {
 
@@ -17,6 +19,7 @@ public class MenuManager {
   private static Array<Menu> menus = new Array<Menu>();
   private static TextureRegion texture;
   private static SpriteBatch spriteBatch;
+  private static boolean disableBackground = false;
 
   public static void setMenu(Menu menu) {
     int i = menus.indexOf(menu, true);
@@ -35,10 +38,27 @@ public class MenuManager {
   }
 
   public static void renderBackground() {
+    if (disableBackground) return;
     if (texture == null) {
-      List<Block> blocks = Blocks.getBlocks();
-      Block block = blocks.get(MathUtils.random(0, blocks.size() - 1));
-      texture = block.getTextureHandler(null).getSide(BlockFace.posX).textureRegion;
+      List<Block> blocks = new ArrayList<Block>();
+      blocks.addAll(Blocks.getBlocks());
+      while (texture == null && blocks.size() > 0) {
+        int index = MathUtils.random(0, blocks.size() - 1);
+        Block block = blocks.get(index);
+        BlockTextureHandler textureHandler;
+        try {
+          textureHandler = block.getTextureHandler(null);
+          if (textureHandler == null) throw new NullPointerException();
+        } catch (Exception e) {
+          blocks.remove(index);
+          continue;
+        }
+        texture = textureHandler.getSide(BlockFace.posX).textureRegion;
+      }
+      if (texture == null) {
+        disableBackground = true;
+        return;
+      }
     }
     if (spriteBatch == null) {
       spriteBatch = new SpriteBatch();
