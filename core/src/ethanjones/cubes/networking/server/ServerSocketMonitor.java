c31fa4d6701b81b326db32d24bae8b5e3983b2de
ictrobot <ictrobot@outlook.com>
2014-12-15 16:42:42 +0000

    Change running boolean in ServerSocketMonitor to an AtomicBoolean

--- core/src/ethanjones/cubes/networking/server/ServerSocketMonitor.java
+++ core/src/ethanjones/cubes/networking/server/ServerSocketMonitor.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.Net.Protocol;
 import com.badlogic.gdx.net.ServerSocket;
 import com.badlogic.gdx.net.Socket;
 import com.badlogic.gdx.utils.Disposable;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import ethanjones.cubes.core.logging.Log;
 import ethanjones.cubes.networking.Networking;
@@ -13,7 +14,7 @@ import ethanjones.cubes.side.Sided;
 
 public class ServerSocketMonitor implements Runnable, Disposable {
 
-  public boolean running;
+  protected final AtomicBoolean running;
   private ServerSocket serverSocket;
   private Thread thread;
   private int port;
@@ -23,18 +24,18 @@ public class ServerSocketMonitor implements Runnable, Disposable {
     this.port = port;
     this.serverNetworking = serverNetworking;
     serverSocket = Gdx.net.newServerSocket(Protocol.TCP, port, Networking.serverSocketHints);
-    running = true;
+    running = new AtomicBoolean(true);
   }
 
   @Override
   public void run() {
     Sided.setSide(Side.Server);
-    while (running) {
+    while (running.get()) {
       try {
         Socket accept = serverSocket.accept(Networking.socketHints);
         ServerConnectionInitializer.check(accept);
       } catch (Exception e) {
-        if (running) Log.error(e);
+        if (running.get()) Log.error(e);
       }
     }
     dispose();
@@ -42,7 +43,7 @@ public class ServerSocketMonitor implements Runnable, Disposable {
 
   @Override
   public void dispose() {
-    running = false;
+    running.set(false);
     serverSocket.dispose();
     getThread().interrupt();
   }
