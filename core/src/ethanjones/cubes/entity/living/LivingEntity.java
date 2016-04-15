ca9cab8b3e07be40c5ea62a10f1dd4e0f753b867
ictrobot <ictrobot@outlook.com>
2016-04-15 17:37:19 +0100

    Remove unimplemented LivingEntity.regenerationSpeed

--- core/src/ethanjones/cubes/entity/living/LivingEntity.java
+++ core/src/ethanjones/cubes/entity/living/LivingEntity.java
@@ -9,25 +9,21 @@ public class LivingEntity extends Entity {
 
   public int health;
   public int maxHealth;
-  public int regenerationSpeed;
 
   public LivingEntity(String type, int maxHealth) {
     super(type);
     this.health = maxHealth;
     this.maxHealth = maxHealth;
-    this.regenerationSpeed = 0;
   }
 
   public LivingEntity(String type, Vector3 position, Vector3 angle, int maxHealth) {
     super(type, position, angle);
     this.health = maxHealth;
     this.maxHealth = maxHealth;
-    this.regenerationSpeed = 0;
   }
 
   public boolean update() {
     return super.update();
-    //TODO regeneration
   }
 
   @Override
@@ -35,7 +31,6 @@ public class LivingEntity extends Entity {
     DataGroup data = super.write();
     data.put("health", health);
     data.put("maxHealth", maxHealth);
-    data.put("regenerationSpeed", regenerationSpeed);
     return data;
   }
 
@@ -44,6 +39,5 @@ public class LivingEntity extends Entity {
     super.read(data);
     health = data.getInteger("health");
     maxHealth = data.getInteger("maxHealth");
-    regenerationSpeed = data.getInteger("regenerationSpeed");
   }
 }
