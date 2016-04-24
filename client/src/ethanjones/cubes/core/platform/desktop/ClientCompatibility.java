21c76ff9e49f4ad3e596276ca0a9e78f5c679a00
ictrobot <ictrobot@outlook.com>
2016-04-24 11:06:46 +0100

    Set icon on desktop

--- client/src/ethanjones/cubes/core/platform/desktop/ClientCompatibility.java
+++ client/src/ethanjones/cubes/core/platform/desktop/ClientCompatibility.java
@@ -2,6 +2,7 @@ package ethanjones.cubes.core.platform.desktop;
 
 import com.badlogic.gdx.Application;
 import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Files;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 
@@ -14,10 +15,17 @@ public class ClientCompatibility extends DesktopCompatibility {
   @Override
   protected void run(ApplicationListener applicationListener) {
     LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+
+    config.addIcon("assets/icon-16x.png", Files.FileType.Internal);
+    config.addIcon("assets/icon-32x.png", Files.FileType.Internal);
+    config.addIcon("assets/icon-64x.png", Files.FileType.Internal);
+    config.addIcon("assets/icon-128x.png", Files.FileType.Internal);
+
     config.vSyncEnabled = false;
     config.foregroundFPS = 0;
     config.backgroundFPS = 0;
     config.forceExit = false;
+
     new LwjglApplication(applicationListener, config);
   }
 }
