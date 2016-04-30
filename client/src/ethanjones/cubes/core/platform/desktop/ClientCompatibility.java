1c2a2c6ab359bde8716015092ade5761fa1cf185
ictrobot <ictrobot@outlook.com>
2016-04-30 13:49:17 +0100

    Add fullscreen mode to desktop (F11)

--- client/src/ethanjones/cubes/core/platform/desktop/ClientCompatibility.java
+++ client/src/ethanjones/cubes/core/platform/desktop/ClientCompatibility.java
@@ -1,12 +1,35 @@
 package ethanjones.cubes.core.platform.desktop;
 
-import com.badlogic.gdx.Application;
-import com.badlogic.gdx.ApplicationListener;
-import com.badlogic.gdx.Files;
+import com.badlogic.gdx.*;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 public class ClientCompatibility extends DesktopCompatibility {
+
+  public final static Comparator<Graphics.DisplayMode> displayModeComparator = new Comparator<Graphics.DisplayMode>() {
+    @Override
+    public int compare(Graphics.DisplayMode o1, Graphics.DisplayMode o2) {
+      int i1 = o1.width * o1.height;
+      int i2 = o2.width * o2.height;
+      if (i1 < i2) return 1;
+      if (i1 > i2) return -1;
+
+      if (o1.refreshRate < o2.refreshRate) return 1;
+      if (o1.refreshRate > o2.refreshRate) return -1;
+
+      if (o1.bitsPerPixel < o2.bitsPerPixel) return 1;
+      if (o1.bitsPerPixel > o2.bitsPerPixel) return -1;
+
+      return 0;
+    }
+  };
+
+  public boolean fullscreen = false;
+  public int windowWidth = 640;
+  public int windowHeight = 480;
 
   protected ClientCompatibility(ClientLauncher clientLauncher, String[] arg) {
     super(clientLauncher, Application.ApplicationType.Desktop, arg);
@@ -24,8 +47,38 @@ public class ClientCompatibility extends DesktopCompatibility {
     config.vSyncEnabled = false;
     config.foregroundFPS = 0;
     config.backgroundFPS = 0;
+
+    config.width = windowWidth;
+    config.height = windowHeight;
+
     config.forceExit = false;
 
     new LwjglApplication(applicationListener, config);
+  }
+
+  @Override
+  public void render() {
+    super.render();
+    if (Gdx.input.isKeyJustPressed(Input.Keys.F11)) {
+      if (fullscreen) {
+        if (windowedMode()) fullscreen = false;
+      } else {
+        if (fullscreenMode()) fullscreen = true;
+      }
+    }
+  }
+
+  public boolean windowedMode() {
+    return Gdx.graphics.setWindowedMode(windowWidth, windowHeight);
+  }
+
+  public boolean fullscreenMode() {
+    windowWidth = Gdx.graphics.getWidth();
+    windowHeight = Gdx.graphics.getHeight();
+
+    Graphics.DisplayMode[] displayModes = Gdx.graphics.getDisplayModes();
+    if (displayModes.length == 0) return false;
+    Arrays.sort(displayModes, displayModeComparator);
+    return Gdx.graphics.setFullscreenMode(displayModes[0]);
   }
 }
