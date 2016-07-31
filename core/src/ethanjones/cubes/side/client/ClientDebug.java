51a2d24b530f7c5b0c597c9d231b7fdcd0113811
ictrobot <ictrobot@outlook.com>
2016-07-31 22:18:18 +0100

    Change ClientDebug loop ms to use raw delta time

--- core/src/ethanjones/cubes/side/client/ClientDebug.java
+++ core/src/ethanjones/cubes/side/client/ClientDebug.java
@@ -14,7 +14,7 @@ import com.badlogic.gdx.math.Vector3;
 
 public class ClientDebug {
 
-  public static class Average {
+  public static class IntAverage {
 
     public int current;
     public long total;
@@ -29,23 +29,39 @@ public class ClientDebug {
     }
   }
 
+  public static class FloatAverage {
+
+    public float current;
+    public long total;
+    public int num;
+    public float average;
+
+    public void add(float i) {
+      current = i;
+      total += i;
+      num++;
+      average = (float) (total / (float) num);
+    }
+  }
+
   private static final String lineSeparator = System.getProperty("line.separator");
-  static Average fps = new Average();
-  static Average loop = new Average();
-  static long lastTime = System.currentTimeMillis();
+  static IntAverage fps = new IntAverage();
+  static FloatAverage loop = new FloatAverage();
+  //static long lastTime = System.currentTimeMillis();
   private static String debugString = "";
 
   public static void update() {
     Vector3 p = Cubes.getClient().player.position;
-    loop.add((int) (System.currentTimeMillis() - lastTime));
-    lastTime = System.currentTimeMillis();
+    loop.add(Gdx.graphics.getRawDeltaTime() * 1000f);
+    //loop.add((int) (System.currentTimeMillis() - lastTime));
+    //lastTime = System.currentTimeMillis();
 
     String str = Branding.VERSION_HASH;
     if (!str.isEmpty()) {
       str = "HASH: " + str + lineSeparator;
     }
 
-    String performance = "FPS:" + fps.current + " AVG:" + fps.average + " MS:" + String.format("%01d", loop.current) + " AVG:" + String.format("%01d", loop.average) + " MEM:" + Compatibility.get().getFreeMemory() + "MB";
+    String performance = "FPS:" + fps.current + " AVG:" + fps.average + " MS:" + String.format("%.3f", loop.current) + " AVG:" + String.format("%.3f", loop.average) + " MEM:" + Compatibility.get().getFreeMemory() + "MB";
     String position = "POS X:" + String.format("%.3f", p.x) + "(" + CoordinateConverter.area(p.x) + ")" + " Y:" + String.format("%.3f", p.y) + "(" + CoordinateConverter.area(p.y) + ")" + " Z:" + String.format("%.3f", p.z) + "(" + CoordinateConverter.area(p.z) + ")";
     String direction = "DIR X:" + String.format("%.3f", Cubes.getClient().player.angle.x) + " Y:" + String.format("%.2f", Cubes.getClient().player.angle.y) + " Z:" + String.format("%.3f", Cubes.getClient().player.angle.z);
     String rendering = "R A:" + AreaRenderer.renderedThisFrame + " M:" + AreaRenderer.renderedMeshesThisFrame;
