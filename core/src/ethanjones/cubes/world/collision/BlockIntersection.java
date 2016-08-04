9d82d7b342a7d463bc96731b21edf9a3ee4b7801
ictrobot <ictrobot@outlook.com>
2016-08-04 21:56:47 +0100

    Better BlockIntersection using "A Fast Voxel Traversal Algorithm for Ray Tracing" http://www.cse.yorku.ca/~amana/research/grid.pdf

--- core/src/ethanjones/cubes/world/collision/BlockIntersection.java
+++ core/src/ethanjones/cubes/world/collision/BlockIntersection.java
@@ -1,32 +1,20 @@
 package ethanjones.cubes.world.collision;
 
+import ethanjones.cubes.block.Block;
 import ethanjones.cubes.core.util.BlockFace;
-import ethanjones.cubes.world.CoordinateConverter;
 import ethanjones.cubes.world.World;
-import ethanjones.cubes.world.reference.AreaReference;
 import ethanjones.cubes.world.reference.BlockReference;
-import ethanjones.cubes.world.storage.Area;
 
-import com.badlogic.gdx.math.Intersector;
 import com.badlogic.gdx.math.Vector3;
-import com.badlogic.gdx.math.collision.BoundingBox;
-import com.badlogic.gdx.math.collision.Ray;
-
-import java.util.*;
-
-import static java.lang.Math.floor;
-import static java.lang.Math.signum;
 
 public class BlockIntersection {
 
   private final BlockReference blockReference;
   private final BlockFace blockFace;
-  private final Vector3 intersection;
 
-  public BlockIntersection(BlockReference blockReference, BlockFace blockFace, Vector3 intersection) {
+  public BlockIntersection(BlockReference blockReference, BlockFace blockFace) {
     this.blockReference = blockReference;
     this.blockFace = blockFace;
-    this.intersection = intersection;
   }
 
   public BlockReference getBlockReference() {
@@ -37,129 +25,111 @@ public class BlockIntersection {
     return blockFace;
   }
 
-  public Vector3 getIntersection() {
-    return intersection;
-  }
-
   public static BlockIntersection getBlockIntersection(Vector3 origin, Vector3 direction, World world) {
     return intersection(origin, direction, 6, world);
   }
 
-  private static AreaReference fastGet = new AreaReference();
-  private static BoundingBox boundingBox = new BoundingBox();
-  private static Ray ray = new Ray();
-
   public static BlockIntersection intersection(Vector3 origin, Vector3 direction, int radius, World world) {
-    Iterator<BlockReference> blocks = getSortedBlockList(origin, direction, radius, world);
+    // A Fast Voxel Traversal Algorithm for Ray Tracing
+    // http://www.cse.yorku.ca/~amana/research/grid.pdf
 
-    ray.set(origin, direction);
-    Vector3 intersection = new Vector3();
+    BlockReference blockReference = new BlockReference().setFromVector3(origin);
+    int stepX, stepY, stepZ;
+    float tMaxX, tMaxY, tMaxZ, tDeltaX, tDeltaY, tDeltaZ, rayLength;
 
-    while (blocks.hasNext()) {
-      BlockReference b = blocks.next();
-      boundingBox.min.set(b.blockX, b.blockY, b.blockZ);
-      boundingBox.max.set(b.blockX + 1, b.blockY + 1, b.blockZ + 1);
-
-      if (Intersector.intersectRayBounds(ray, boundingBox, intersection)) {
-        BlockFace blockFace = getBlockFace(b, intersection);
-        return new BlockIntersection(b, blockFace, intersection);
-      }
+    if (direction.x > 0) {
+      stepX = 1;
+      tDeltaX = 1 / direction.x;
+      tMaxX = (blockReference.blockX + 1 - origin.x) * tDeltaX;
+    } else if (direction.x < 0) {
+      stepX = -1;
+      tDeltaX = 1 / -direction.x;
+      tMaxX = (origin.x - blockReference.blockX) * tDeltaX;
+    } else {
+      stepX = 0;
+      tDeltaX = 0;
+      tMaxX = Float.MAX_VALUE;
     }
+
+    if (direction.y > 0) {
+      stepY = 1;
+      tDeltaY = 1 / direction.y;
+      tMaxY = (blockReference.blockY + 1 - origin.y) * tDeltaY;
+    } else if (direction.y < 0) {
+      stepY = -1;
+      tDeltaY = 1 / -direction.y;
+      tMaxY = (origin.y - blockReference.blockY) * tDeltaY;
+    } else {
+      stepY = 0;
+      tDeltaY = 0;
+      tMaxY = Float.MAX_VALUE;
+    }
+
+    if (direction.z > 0) {
+      stepZ = 1;
+      tDeltaZ = 1 / direction.z;
+      tMaxZ = (blockReference.blockZ + 1 - origin.z) * tDeltaZ;
+    } else if (direction.z < 0) {
+      stepZ = -1;
+      tDeltaZ = 1 / -direction.z;
+      tMaxZ = (origin.z - blockReference.blockZ) * tDeltaZ;
+    } else {
+      stepZ = 0;
+      tDeltaZ = 0;
+      tMaxZ = Float.MAX_VALUE;
+    }
+
+    do {
+      XYZ side;
+      if (tMaxX < tMaxY) {
+        if (tMaxX < tMaxZ) {
+          rayLength = tMaxX;
+          side = XYZ.x;
+          blockReference.blockX += stepX;
+          tMaxX = tMaxX + tDeltaX;
+        } else {
+          rayLength = tMaxZ;
+          side = XYZ.z;
+          blockReference.blockZ += stepZ;
+          tMaxZ = tMaxZ + tDeltaZ;
+        }
+      } else {
+        if (tMaxY < tMaxZ) {
+          rayLength = tMaxY;
+          side = XYZ.y;
+          blockReference.blockY += stepY;
+          tMaxY = tMaxY + tDeltaY;
+        } else {
+          rayLength = tMaxZ;
+          side = XYZ.z;
+          blockReference.blockZ += stepZ;
+          tMaxZ = tMaxZ + tDeltaZ;
+        }
+      }
+
+      if (rayLength < radius) {
+        Block block = world.getBlock(blockReference.blockX, blockReference.blockY, blockReference.blockZ);
+        if (block != null) {
+          BlockFace blockFace = null;
+          if (side == XYZ.x) {
+            if (stepX > 0) blockFace = BlockFace.negX;
+            if (stepX < 0) blockFace = BlockFace.posX;
+          } else if (side == XYZ.y) {
+            if (stepY > 0) blockFace = BlockFace.negY;
+            if (stepY < 0) blockFace = BlockFace.posY;
+          } else if (side == XYZ.z) {
+            if (stepZ > 0) blockFace = BlockFace.negZ;
+            if (stepZ < 0) blockFace = BlockFace.posZ;
+          }
+          return new BlockIntersection(blockReference, blockFace);
+        }
+      }
+    } while (rayLength < radius);
+
     return null;
   }
 
-  private static BlockFace getBlockFace(BlockReference b, Vector3 i) {
-    i.sub(b.blockX, b.blockY, b.blockZ);
-    if (i.x == 1f) return BlockFace.posX;
-    if (i.x == 0f) return BlockFace.negX;
-    if (i.y == 1f) return BlockFace.posY;
-    if (i.y == 0f) return BlockFace.negY;
-    if (i.z == 1f) return BlockFace.posZ;
-    if (i.z == 0f) return BlockFace.negZ;
-    return null;
-  }
-
-  private static Iterator<BlockReference> getSortedBlockList(final Vector3 origin, final Vector3 direction, int radius, World world) {
-    int initialX = (int) floor(origin.x);
-    int initialY = (int) floor(origin.y);
-    int initialZ = (int) floor(origin.z);
-
-    int stepX = (int) signum(direction.x);
-    int stepY = (int) signum(direction.y);
-    int stepZ = (int) signum(direction.z);
-
-    final ArrayList<BlockReference> list = new ArrayList<BlockReference>();
-
-    world.lock.readLock();
-    int currentAreaX = Integer.MIN_VALUE;
-    int currentAreaZ = Integer.MIN_VALUE;
-    Area area = null;
-    for (int nX = 0; nX <= (stepX != 0 ? radius : 0); nX++) {
-      for (int nY = 0; nY <= (stepY != 0 ? radius : 0); nY++) {
-        for (int nZ = 0; nZ <= (stepZ != 0 ? radius : 0); nZ++) {
-          int x = initialX + (nX * stepX);
-          int y = initialY + (nY * stepY);
-          int z = initialZ + (nZ * stepZ);
-
-          int areaX = CoordinateConverter.area(x);
-          int areaZ = CoordinateConverter.area(z);
-
-          if (areaX != currentAreaX || areaZ != currentAreaZ) {
-            currentAreaX = areaX;
-            currentAreaZ = areaZ;
-
-            Area fastGet = fastGet(world, currentAreaX, currentAreaZ);
-            if (area != null) area.lock.readUnlock();
-            area = fastGet;
-            if (area != null) area.lock.readLock();
-          }
-
-          if (area != null && area.isReady() && y <= area.maxY && y >= 0 && area.blocks[Area.getRef(x - area.minBlockX, y, z - area.minBlockZ)] != 0) {
-            list.add(new BlockReference().setFromBlockCoordinates(x, y, z));
-          }
-        }
-      }
-    }
-    if (area != null) area.lock.readUnlock();
-    world.lock.readUnlock();
-
-    return new Iterator<BlockReference>() {
-      @Override
-      public boolean hasNext() {
-        return list.size() > 0;
-      }
-
-      @Override
-      public BlockReference next() {
-        BlockReference current = null;
-        float currentDistance2 = Float.POSITIVE_INFINITY;
-        for (BlockReference blockReference : list) {
-          float distance2 = distance2(blockReference);
-          if (distance2 < currentDistance2) {
-            currentDistance2 = distance2;
-            current = blockReference;
-          }
-        }
-        list.remove(current);
-        return current;
-      }
-
-      public float distance2(BlockReference b) {
-        float dx = b.blockX - origin.x;
-        float dy = b.blockY - origin.y;
-        float dz = b.blockZ - origin.z;
-        return (dx * dx) + (dy * dy) + (dz * dz);
-      }
-
-      @Override
-      public void remove() {
-        throw new UnsupportedOperationException();
-      }
-    };
-  }
-
-  private static Area fastGet(World world, int x, int z) {
-    fastGet.setFromAreaCoordinates(x, z);
-    return world.map.get(fastGet);
+  private enum XYZ {
+    x, y, z;
   }
 }
