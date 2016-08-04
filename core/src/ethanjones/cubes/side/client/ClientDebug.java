20dee987a4048a2b6f408f718a2781aa68132d6f
ictrobot <ictrobot@outlook.com>
2016-08-04 13:55:23 +0100

    Change ClientDebug to use StringBuilder

--- core/src/ethanjones/cubes/side/client/ClientDebug.java
+++ core/src/ethanjones/cubes/side/client/ClientDebug.java
@@ -49,6 +49,8 @@ public class ClientDebug {
   static FloatAverage loop = new FloatAverage();
   //static long lastTime = System.currentTimeMillis();
   private static String debugString = "";
+  private static StringBuilder builder = new StringBuilder(250).append(Branding.DEBUG).append(lineSeparator);
+  private static int brandingDebugLength = builder.length();
 
   public static void update() {
     Vector3 p = Cubes.getClient().player.position;
@@ -61,12 +63,20 @@ public class ClientDebug {
       str = "HASH: " + str + lineSeparator;
     }
 
-    String performance = "FPS:" + fps.current + " AVG:" + fps.average + " MS:" + String.format("%.3f", loop.current) + " AVG:" + String.format("%.3f", loop.average) + " MEM:" + Compatibility.get().getFreeMemory() + "MB";
-    String position = "POS X:" + String.format("%.3f", p.x) + "(" + CoordinateConverter.area(p.x) + ")" + " Y:" + String.format("%.3f", p.y) + "(" + CoordinateConverter.area(p.y) + ")" + " Z:" + String.format("%.3f", p.z) + "(" + CoordinateConverter.area(p.z) + ")";
-    String direction = "DIR X:" + String.format("%.3f", Cubes.getClient().player.angle.x) + " Y:" + String.format("%.2f", Cubes.getClient().player.angle.y) + " Z:" + String.format("%.3f", Cubes.getClient().player.angle.z);
-    String rendering = "R A:" + AreaRenderer.renderedThisFrame + " M:" + AreaRenderer.renderedMeshesThisFrame;
-    String world = "W B:" + getBlockLight() + " S:" + getSunlight() + " T:" + Cubes.getClient().world.time;
-    debugString = Branding.DEBUG + lineSeparator + performance + lineSeparator + position + lineSeparator + direction + lineSeparator + rendering + lineSeparator + world;
+    //String performance = "FPS:" + fps.current + " AVG:" + fps.average + " MS:" + String.format("%.3f", loop.current) + " AVG:" + String.format("%.3f", loop.average) + " MEM:" + Compatibility.get().getFreeMemory() + "MB";
+    //String position = "POS X:" + String.format("%.3f", p.x) + "(" + CoordinateConverter.area(p.x) + ")" + " Y:" + String.format("%.3f", p.y) + "(" + CoordinateConverter.area(p.y) + ")" + " Z:" + String.format("%.3f", p.z) + "(" + CoordinateConverter.area(p.z) + ")";
+    //String direction = "DIR X:" + String.format("%.3f", Cubes.getClient().player.angle.x) + " Y:" + String.format("%.2f", Cubes.getClient().player.angle.y) + " Z:" + String.format("%.3f", Cubes.getClient().player.angle.z);
+    //String rendering = "R A:" + AreaRenderer.renderedThisFrame + " M:" + AreaRenderer.renderedMeshesThisFrame;
+    //String world = "W B:" + getBlockLight() + " S:" + getSunlight() + " T:" + Cubes.getClient().world.time;
+    //debugString = Branding.DEBUG + lineSeparator + performance + lineSeparator + position + lineSeparator + direction + lineSeparator + rendering + lineSeparator + world;
+
+    builder.setLength(brandingDebugLength);
+    builder.append("FPS:").append(fps.current).append(" AVG:").append(fps.average).append(" MS:").append(String.format("%.3f", loop.current)).append(" AVG:").append(String.format("%.3f", loop.average)).append(" MEM:").append(Compatibility.get().getFreeMemory()).append("MB").append(lineSeparator);
+    builder.append("POS X:").append(String.format("%.3f", p.x)).append("(").append(CoordinateConverter.area(p.x)).append(")").append(" Y:").append(String.format("%.3f", p.y)).append("(").append(CoordinateConverter.area(p.y)).append(")").append(" Z:").append(String.format("%.3f", p.z)).append("(").append(CoordinateConverter.area(p.z)).append(")").append(lineSeparator);
+    builder.append("DIR X:").append(String.format("%.3f", Cubes.getClient().player.angle.x)).append(" Y:").append(String.format("%.2f", Cubes.getClient().player.angle.y)).append(" Z:").append(String.format("%.3f", Cubes.getClient().player.angle.z)).append(lineSeparator);
+    builder.append("R A:").append(AreaRenderer.renderedThisFrame).append(" M:").append(AreaRenderer.renderedMeshesThisFrame).append(lineSeparator);
+    builder.append("W B:").append(getBlockLight()).append(" S:").append(getSunlight()).append(" T:").append(Cubes.getClient().world.time);
+    debugString = builder.toString();
 
     GLProfiler.calls = 0;
     GLProfiler.drawCalls = 0;
