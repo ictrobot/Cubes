7f180653d936f634f327a30f099eb0317304b4da
ictrobot <ictrobot@outlook.com>
2016-08-17 13:09:29 +0100

    Fix InventoryManagerTouchscreen for Creative Inventory

--- core/src/ethanjones/cubes/graphics/hud/inv/InventoryManagerTouchscreen.java
+++ core/src/ethanjones/cubes/graphics/hud/inv/InventoryManagerTouchscreen.java
@@ -10,29 +10,37 @@ public class InventoryManagerTouchscreen {
   private static SlotActor selected;
 
   protected static void newSlot(final SlotActor slot) {
-    final Inventory inventory = slot.getInventory();
-    final int num = slot.getNum();
+    final Inventory targetInv = slot.getInventory();
+    final int targetNum = slot.getNum();
 
     slot.addListener(new ClickListener() {
 
       @Override
       public void clicked(InputEvent event, float x, float y) {
         if (selected == null) {
-          if (inventory.itemStacks[num] != null && getTapCount() == 2) {
+          if (targetInv.itemStacks[targetNum] != null && getTapCount() == 2) {
             selected = slot;
           }
         } else {
           Inventory selectedInv = selected.getInventory();
           int selectedNum = selected.getNum();
 
-          ItemStack itemStack = inventory.itemStacks[num];
-          inventory.itemStacks[num] = selectedInv.itemStacks[selectedNum];
-          selectedInv.itemStacks[selectedNum] = itemStack;
-
-          if (selectedInv == inventory) {
-            inventory.sync();
+          ItemStack itemStack = targetInv.itemStacks[targetNum];
+          if (targetInv.fixed) {
+            if (targetInv.voidItems && !selectedInv.fixed) {
+              selectedInv.itemStacks[selectedNum] = null;
+            }
           } else {
-            inventory.sync();
+            targetInv.itemStacks[targetNum] = selectedInv.itemStacks[selectedNum];
+            if (!selectedInv.fixed) {
+              selectedInv.itemStacks[selectedNum] = itemStack;
+            }
+          }
+
+          if (selectedInv == targetInv) {
+            targetInv.sync();
+          } else {
+            targetInv.sync();
             selectedInv.sync();
           }
           selected = null;
