d319a9a3950efa716cbc112273af56fc25cf4d1f
ictrobot <ictrobot@outlook.com>
2016-08-20 12:22:34 +0100

    Fix Hotbar scrolling

--- core/src/ethanjones/cubes/graphics/hud/inv/HotbarActor.java
+++ core/src/ethanjones/cubes/graphics/hud/inv/HotbarActor.java
@@ -39,6 +39,7 @@ public class HotbarActor extends Image {
       public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
         int slot = (int) Math.floor((event.getStageX() - getX()) / 48f);
         playerInventory.hotbarSelected = slot;
+        playerInventory.sync();
         return true;
       }
     });
