a1d838df59ff18632f1c02c1a123aa11ec3da44e
ictrobot <ictrobot@outlook.com>
2017-07-16 22:16:19 +0100

    Disable sonarqube until it works with android gradle build tools 3.0.0, reduce spam in log from downloading android sdk

--- android/travis_androidsdk.sh
+++ android/travis_androidsdk.sh
@@ -2,7 +2,7 @@
 
 mkdir androidsdk
 cd androidsdk
-wget https://dl.google.com/android/repository/sdk-tools-linux-3859397.zip -O sdk.zip
+curl https://dl.google.com/android/repository/sdk-tools-linux-3859397.zip -o sdk.zip
 unzip sdk.zip
 rm sdk.zip
 yes | ./tools/bin/sdkmanager --licenses
