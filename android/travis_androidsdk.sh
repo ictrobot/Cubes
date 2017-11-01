f672aabe6b29b2de0cd46ea1d5cf830dc832b49e
ictrobot <ictrobot@outlook.com>
2017-11-01 17:14:58 +0000

    Fix travis build

---
+++ android/travis_androidsdk.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+mkdir androidsdk
+cd androidsdk
+curl https://dl.google.com/android/repository/sdk-tools-linux-3859397.zip -o sdk.zip
+unzip -q sdk.zip
+rm sdk.zip
+yes | ./tools/bin/sdkmanager --licenses
+echo sdk.dir=`pwd` >> ../local.properties
+cd ..
