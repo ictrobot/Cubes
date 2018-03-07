caf413febdb583d5203f1ebf4730c7e9be458720
ictrobot <ictrobot@outlook.com>
2018-03-07 19:20:02 +0000

    Improve Frametime Graph

--- core/src/ethanjones/cubes/graphics/hud/FrametimeGraph.java
+++ core/src/ethanjones/cubes/graphics/hud/FrametimeGraph.java
@@ -20,6 +20,7 @@ public class FrametimeGraph {
   private static LinkedList<Float> frametimes = new LinkedList<Float>();
   private static Texture textureLine = Assets.getTexture("core:hud/FrametimeGraphLine.png");
   private static Texture textureLine60 = Assets.getTexture("core:hud/FrametimeGraphLine60.png");
+  private static Texture textureBackground = Assets.getTexture("core:hud/FrametimeGraphBackground.png");
   private static final int scale = 10;
   private static final float yOffset = 50f;
   private static final ImmediateModeRenderer renderer = new ImmediateModeRenderer20(2000, false, false, 0, createShaderProgram());
@@ -28,11 +29,12 @@ public class FrametimeGraph {
     if (!Settings.getBooleanSettingValue(Settings.DEBUG_FRAMETIME_GRAPH)) return;
 
     float width = width();
-    
+
+    batch.draw(textureBackground, 0, yOffset, width, scale * 32);
     while (frametimes.size() < width) {
       frametimes.addFirst(0f);
     }
-    for (int i = 0; i <= 16; i++) {
+    for (int i = 0; i <= 32; i++) {
       batch.draw(textureLine, 0, yOffset + (scale * i), width, 1);
     }
     batch.draw(textureLine60, 0, yOffset + (scale * 16.666666f), width, 1);
@@ -43,7 +45,9 @@ public class FrametimeGraph {
 
     renderer.begin(Graphics.screenViewport.getCamera().combined, GL20.GL_POINTS);
     for (int x = 0; x < Math.min(frametimes.size(), width()); x++) {
-      float y = yOffset + (frametimes.get(x) * scale);
+      float value = frametimes.get(x);
+      if (value > 32) value = 32;
+      float y = yOffset + (value * scale);
       renderer.vertex(x, y, 0);
     }
     renderer.end();
@@ -67,7 +71,7 @@ public class FrametimeGraph {
     vertex += "   gl_PointSize = 1.0;\n";
     vertex += "}\n";
     String fragment = "#ifdef GL_ES\n" + "precision mediump float;\n" + "#endif\n";
-    fragment += "void main() {\n" + "   gl_FragColor = vec4(0, 1, 0, 1)"; //#00ff00ff
+    fragment += "void main() {\n" + "   gl_FragColor = vec4(1, 0, 0, 1)"; //#00ff00ff
     fragment += ";\n}";
     return new ShaderProgram(vertex, fragment);
   }
