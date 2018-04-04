c3eb7cf9e470b3b5512133716e74e32ec81b4fbc
ictrobot <ictrobot@outlook.com>
2018-04-04 16:15:22 +0100

    Optimize CubesRenderableSorter

--- core/src/ethanjones/cubes/graphics/world/CubesModelBatch.java
+++ core/src/ethanjones/cubes/graphics/world/CubesModelBatch.java
@@ -8,7 +8,6 @@ import com.badlogic.gdx.graphics.g3d.ModelBatch;
 import com.badlogic.gdx.graphics.g3d.Renderable;
 import com.badlogic.gdx.graphics.g3d.attributes.BlendingAttribute;
 import com.badlogic.gdx.graphics.g3d.utils.RenderableSorter;
-import com.badlogic.gdx.math.Matrix4;
 import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.utils.Array;
 
@@ -30,22 +29,12 @@ public class CubesModelBatch extends ModelBatch {
       renderables.sort(this);
     }
 
-    private void getTranslation(Matrix4 worldTransform, Vector3 center, Vector3 output) {
-      if (center.isZero()) {
-        worldTransform.getTranslation(output);
-      } else if (!worldTransform.hasRotationOrScaling()) {
-        worldTransform.getTranslation(output).add(center);
-      } else {
-        output.set(center).mul(worldTransform);
-      }
-    }
-
     @Override
     public int compare(final Renderable o1, final Renderable o2) {
       if (o1.material == o2.material) {
-        // when materials are the same, quicker path
-        getTranslation(o1.worldTransform, o1.meshPart.center, translation1);
-        getTranslation(o2.worldTransform, o2.meshPart.center, translation2);
+        // when materials are the same, use this quicker path
+        translation1.set(o1.meshPart.center).mul(o1.worldTransform);
+        translation2.set(o1.meshPart.center).mul(o2.worldTransform);
         final float dst = (int) (1000f * camera.position.dst2(translation1)) - (int) (1000f * camera.position.dst2(translation2));
         return dst < 0 ? 1 : (dst > 0 ? -1 : 0);
       }
@@ -54,8 +43,8 @@ public class CubesModelBatch extends ModelBatch {
       final boolean b2 = o2.material.has(BlendingAttribute.Type) && ((BlendingAttribute) o2.material.get(BlendingAttribute.Type)).blended;
       if (b1 != b2) return b1 ? 1 : -1;
 
-      getTranslation(o1.worldTransform, o1.meshPart.center, translation1);
-      getTranslation(o2.worldTransform, o2.meshPart.center, translation2);
+      translation1.set(o1.meshPart.center).mul(o1.worldTransform);
+      translation2.set(o1.meshPart.center).mul(o2.worldTransform);
       final float dst = (int) (1000f * camera.position.dst2(translation1)) - (int) (1000f * camera.position.dst2(translation2));
       final int result = dst < 0 ? -1 : (dst > 0 ? 1 : 0);
       return b1 ? -result : result;
