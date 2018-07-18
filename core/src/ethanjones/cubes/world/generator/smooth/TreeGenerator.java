2c6a5960183c66a9dfde116e4cfbde7a99c03f90
ictrobot <ictrobot@outlook.com>
2018-07-18 20:36:02 +0100

    Revert "Change BlockLeaves to have a more efficient random tick handler"
    This reverts commit 8783f82

--- core/src/ethanjones/cubes/world/generator/smooth/TreeGenerator.java
+++ core/src/ethanjones/cubes/world/generator/smooth/TreeGenerator.java
@@ -7,55 +7,62 @@ import ethanjones.cubes.world.storage.Area;
 public class TreeGenerator {
   
   public void generateTree(int x, int y, int z, int h, Area area) {
-    setBlock(area, Blocks.leaves, x - 1, y + h, z - 2, 3);
-    setBlock(area, Blocks.leaves, x + 0, y + h, z - 2, 2);
-    setBlock(area, Blocks.leaves, x + 1, y + h, z - 2, 3);
+    //setBlock(area, Blocks.leaves, x - 2, y + h, z - 2);
+    setBlock(area, Blocks.leaves, x - 1, y + h, z - 2, 1);
+    setBlock(area, Blocks.leaves, x + 0, y + h, z - 2, 1);
+    setBlock(area, Blocks.leaves, x + 1, y + h, z - 2, 1);
+    //setBlock(area, Blocks.leaves, x + 2, y + h, z - 2);
   
-    setBlock(area, Blocks.leaves, x - 2, y + h, z - 1, 3);
-    setBlock(area, Blocks.leaves, x - 1, y + h, z - 1, 2);
+    setBlock(area, Blocks.leaves, x - 2, y + h, z - 1, 1);
+    setBlock(area, Blocks.leaves, x - 1, y + h, z - 1, 1);
     setBlock(area, Blocks.leaves, x + 0, y + h, z - 1, 1);
-    setBlock(area, Blocks.leaves, x + 1, y + h, z - 1, 2);
-    setBlock(area, Blocks.leaves, x + 2, y + h, z - 1, 3);
+    setBlock(area, Blocks.leaves, x + 1, y + h, z - 1, 1);
+    setBlock(area, Blocks.leaves, x + 2, y + h, z - 1, 1);
   
-    setBlock(area, Blocks.leaves, x - 2, y + h, z + 0, 2);
+    setBlock(area, Blocks.leaves, x - 2, y + h, z + 0, 1);
     setBlock(area, Blocks.leaves, x - 1, y + h, z + 0, 1);
+    setBlock(area, Blocks.leaves, x + 0, y + h, z + 0, 1);
     setBlock(area, Blocks.leaves, x + 1, y + h, z + 0, 1);
-    setBlock(area, Blocks.leaves, x + 2, y + h, z + 0, 2);
+    setBlock(area, Blocks.leaves, x + 2, y + h, z + 0, 1);
   
-    setBlock(area, Blocks.leaves, x - 2, y + h, z + 1, 3);
-    setBlock(area, Blocks.leaves, x - 1, y + h, z + 1, 2);
+    setBlock(area, Blocks.leaves, x - 2, y + h, z + 1, 1);
+    setBlock(area, Blocks.leaves, x - 1, y + h, z + 1, 1);
     setBlock(area, Blocks.leaves, x + 0, y + h, z + 1, 1);
-    setBlock(area, Blocks.leaves, x + 1, y + h, z + 1, 2);
-    setBlock(area, Blocks.leaves, x + 2, y + h, z + 1, 3);
-
-    setBlock(area, Blocks.leaves, x - 1, y + h, z + 2, 3);
-    setBlock(area, Blocks.leaves, x + 0, y + h, z + 2, 2);
-    setBlock(area, Blocks.leaves, x + 1, y + h, z + 2, 3);
+    setBlock(area, Blocks.leaves, x + 1, y + h, z + 1, 1);
+    setBlock(area, Blocks.leaves, x + 2, y + h, z + 1, 1);
+  
+    //setBlock(area, Blocks.leaves, x - 2, y + h, z + 2);
+    setBlock(area, Blocks.leaves, x - 1, y + h, z + 2, 1);
+    setBlock(area, Blocks.leaves, x + 0, y + h, z + 2, 1);
+    setBlock(area, Blocks.leaves, x + 1, y + h, z + 2, 1);
+    //setBlock(area, Blocks.leaves, x + 2, y + h, z + 2);
   
     //Second layer
   
-    setBlock(area, Blocks.leaves, x - 2, y + h + 1, z + 0, 2);
+    setBlock(area, Blocks.leaves, x - 2, y + h + 1, z + 0, 1);
     setBlock(area, Blocks.leaves, x - 1, y + h + 1, z + 0, 1);
+    setBlock(area, Blocks.leaves, x + 0, y + h + 1, z + 0, 1);
     setBlock(area, Blocks.leaves, x + 1, y + h + 1, z + 0, 1);
-    setBlock(area, Blocks.leaves, x + 2, y + h + 1, z + 0, 2);
+    setBlock(area, Blocks.leaves, x + 2, y + h + 1, z + 0, 1);
   
-    setBlock(area, Blocks.leaves, x + 0, y + h + 1, z - 2, 2);
+    setBlock(area, Blocks.leaves, x + 0, y + h + 1, z - 2, 1);
     setBlock(area, Blocks.leaves, x + 0, y + h + 1, z - 1, 1);
+    setBlock(area, Blocks.leaves, x + 0, y + h + 1, z + 0, 1);
     setBlock(area, Blocks.leaves, x + 0, y + h + 1, z + 1, 1);
-    setBlock(area, Blocks.leaves, x + 0, y + h + 1, z + 2, 2);
+    setBlock(area, Blocks.leaves, x + 0, y + h + 1, z + 2, 1);
   
-    setBlock(area, Blocks.leaves, x + 1, y + h + 1, z + 1, 2);
-    setBlock(area, Blocks.leaves, x + 1, y + h + 1, z - 1, 2);
-    setBlock(area, Blocks.leaves, x - 1, y + h + 1, z + 1, 2);
-    setBlock(area, Blocks.leaves, x - 1, y + h + 1, z - 1, 2);
+    setBlock(area, Blocks.leaves, x + 1, y + h + 1, z + 1, 1);
+    setBlock(area, Blocks.leaves, x + 1, y + h + 1, z - 1, 1);
+    setBlock(area, Blocks.leaves, x - 1, y + h + 1, z + 1, 1);
+    setBlock(area, Blocks.leaves, x - 1, y + h + 1, z - 1, 1);
   
     // Third layer
   
     setBlock(area, Blocks.leaves, x + 0, y + h + 2, z + 0, 1);
-    setBlock(area, Blocks.leaves, x + 0, y + h + 2, z + 1, 2);
-    setBlock(area, Blocks.leaves, x + 0, y + h + 2, z - 1, 2);
-    setBlock(area, Blocks.leaves, x + 1, y + h + 2, z + 0, 2);
-    setBlock(area, Blocks.leaves, x - 1, y + h + 2, z + 0, 2);
+    setBlock(area, Blocks.leaves, x + 0, y + h + 2, z + 1, 1);
+    setBlock(area, Blocks.leaves, x + 0, y + h + 2, z - 1, 1);
+    setBlock(area, Blocks.leaves, x + 1, y + h + 2, z + 0, 1);
+    setBlock(area, Blocks.leaves, x - 1, y + h + 2, z + 0, 1);
   
     for (int i = 0; i < h + 2; i++) {
       setBlock(area, Blocks.log, x, y + i, z, 0);
